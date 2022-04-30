08cfc4de7b0cbf9f3a93be577e4594d77db21c93
saleh <salehjalili36@gmail.com>
2022-04-30 15:21:40 +0430

    initial

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+// Hooks
+import { useState } from "react";
 // Styles
 import styles from "./Navbar.module.scss";
 // Assets
@@ -13,22 +15,32 @@ const navigation = [
 ];
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.navbar_container}>
       <div className={styles.navbar_logo_container}>
-        <Link href="/"> 
+        <Link href="/">
           <a>
             <Image src={logo} alt="logo" />
           </a>
         </Link>
       </div>
-      <div className={styles.navbar_bars_container}>
-        <i className="fa fa-bars"></i>
+      <div className={styles.navbar_bars_container} onClick={toggleMenu}>
+        {isOpen ? (
+          <i className="fa fa-times"></i>
+        ) : (
+          <i className="fa fa-bars"></i>
+        )}
       </div>
-      <div className={styles.nav_items_container}>
+      <div className={`${styles.nav_items_container} ${isOpen && styles.active}`}>
         <ul>
           {navigation.map((nav) => (
-            <li key={nav.name}>
+            <li key={nav.name} onClick={() => setIsOpen(false)}>
               <Link href={nav.href}>
                 <a>{nav.name}</a>
               </Link>
@@ -36,7 +48,7 @@ function Navbar() {
           ))}
         </ul>
         <Link href="signup">
-          <a className={styles.signup}>Signup</a>
+          <a className={styles.signup} onClick={() => setIsOpen(false)}>Signup</a>
         </Link>
       </div>
     </div>
